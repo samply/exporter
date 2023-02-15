47834df3e45cddc57e7b0e8ea1915f9eaeee396d
juarez <d.juarez@dkfz-heidelberg.de>
2023-02-15 19:17:53 +0100

    Bugfix: Filter paths

--- src/main/java/de/samply/csv/Session.java
+++ src/main/java/de/samply/csv/Session.java
@@ -4,13 +4,17 @@ import de.samply.template.ContainerTemplate;
 import de.samply.template.ConverterTemplateUtils;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Session {
 
   private ConverterTemplateUtils converterTemplateUtils;
   private String writeDirectory;
+
+  private List<Path> pathList = new ArrayList<>();
 
   public Session(ConverterTemplateUtils converterTemplateUtils,
       String writeDirectory) {
@@ -27,6 +31,28 @@ public class Session {
       filenameMap.put(containerTemplate.getCsvFilename(), filename);
     }
     return Paths.get(writeDirectory).resolve(filename);
+  }
+
+  public List<Path> getNewPaths(List<Path> pathList) {
+    List<Path> results = new ArrayList<>();
+    if (pathList != null && pathList.size() > 0) {
+      pathList.forEach(path -> {
+        if (isNew(path)) {
+          results.add(path);
+          this.pathList.add(path);
+        }
+      });
+    }
+    return results;
+  }
+
+  private boolean isNew(Path path){
+    for (Path tempPath: pathList){
+      if (tempPath.compareTo(path) == 0){
+        return false;
+      }
+    }
+    return true;
   }
 
 }
