d30c3eb5be9a4b225cccf209b77c2a7b8d50bd98
juarez <d.juarez@dkfz-heidelberg.de>
2023-06-21 16:31:08 +0200

    Deactivate hapi fhir client and hibernate logs (configurable)

--- src/main/java/de/samply/exporter/ExporterConst.java
+++ src/main/java/de/samply/exporter/ExporterConst.java
@@ -192,6 +192,7 @@ public class ExporterConst {
   public static final String HTTP_HEADER_CONTENT_DISPOSITION = "Content-Disposition";
   public static final String HTTP_HEADER_CONTENT_DISPOSITION_FILENAME = "filename=\"";
   public static final int RANDOM_FILENAME_SIZE = 10;
+  public static final String BLAZE_URL_QUERY_PARAMETER_TOTAL_OF_PAGES = "__t=";
 
 
 }
