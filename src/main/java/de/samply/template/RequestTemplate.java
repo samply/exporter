5bd42e537be8d511510d79bc6a43d001da4eaf9b
juarez <d.juarez@dkfz-heidelberg.de>
2023-11-30 18:21:19 +0100

    Added: request template

---
+++ src/main/java/de/samply/template/RequestTemplate.java
@@ -0,0 +1,7 @@
+package de.samply.template;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+@JacksonXmlRootElement(localName = "request")
+public class RequestTemplate {
+}
