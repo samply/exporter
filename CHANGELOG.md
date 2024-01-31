db8e81eff0ce7d9c1c93c2c283aaa598808be2f4
juarez <d.juarez@dkfz-heidelberg.de>
2024-01-31 17:10:15 +0100

    Fixed: Opal Views

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## [1.1.0 - 2023-12-24]
+## [1.1.0 - 2024-01-31]
 ### Added
 - Java Opts
 - Divide Excel file in several files if number of rows is too large
@@ -63,6 +63,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 - Is same resource
 - Rest service: Get output formats
 - Extract parameters of HttpServletRequest
+- Opal Views
 
 ## [1.0.0 - 2023-07-20]
 ### Added
--- src/main/java/de/samply/opal/ViewFactory.java
+++ src/main/java/de/samply/opal/ViewFactory.java
@@ -7,6 +7,7 @@ import de.samply.opal.model.Variable;
 import de.samply.opal.model.View;
 import de.samply.template.AttributeTemplate;
 import de.samply.template.ContainerTemplate;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,73 +16,65 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ViewFactory {
 
 
-  public static View createView(Session session, ContainerTemplate containerTemplate) {
-    View view = new View();
-    view.setName(OpalUtils.fetchViewName(containerTemplate));
-    view.setFrom(fetchViewFrom(session, containerTemplate));
-    view.setMagmaView(createMagmaView(session, containerTemplate));
-    return view;
-  }
+    public static View createView(Session session, ContainerTemplate containerTemplate) {
+        View view = new View();
+        view.setName(OpalUtils.fetchViewName(containerTemplate));
+        view.setFrom(fetchViewFrom(session, containerTemplate));
+        view.setMagmaView(createMagmaView(session, containerTemplate));
+        return view;
+    }
 
-  private static List<String> fetchViewFrom(Session session, ContainerTemplate containerTemplate) {
-    return Arrays.asList(session.fetchProject() + '.' + containerTemplate.getOpalTable());
-  }
+    private static List<String> fetchViewFrom(Session session, ContainerTemplate containerTemplate) {
+        return Arrays.asList(session.fetchProject() + '.' + containerTemplate.getOpalTable());
+    }
 
-  private static MagmaView createMagmaView(Session session, ContainerTemplate containerTemplate) {
-    MagmaView magmaView = new MagmaView();
-    List<Variable> variables = new ArrayList<>();
-    AtomicInteger counter = new AtomicInteger(0);
-    containerTemplate.getAttributeTemplates().forEach(attributeTemplate -> variables.add(
-        createVariable(session, containerTemplate, attributeTemplate, counter.getAndIncrement())));
-    magmaView.setVariables(variables);
-    return magmaView;
-  }
+    private static MagmaView createMagmaView(Session session, ContainerTemplate containerTemplate) {
+        MagmaView magmaView = new MagmaView();
+        List<Variable> variables = new ArrayList<>();
+        AtomicInteger counter = new AtomicInteger(0);
+        containerTemplate.getAttributeTemplates().stream().filter(attributeTemplate -> !attributeTemplate.isPrimaryKey()).forEach(attributeTemplate -> variables.add(
+                createVariable(session, containerTemplate, attributeTemplate, counter.getAndIncrement())));
+        magmaView.setVariables(variables);
+        return magmaView;
+    }
 
-  private static Variable createVariable(Session session, ContainerTemplate containerTemplate,
-      AttributeTemplate attributeTemplate, int counter) {
-    Variable variable = new Variable();
-    variable.setIndex(counter);
-    variable.setName(attributeTemplate.getCsvColumnName());
-    variable.setValueType(attributeTemplate.getOpalValueType());
-    variable.setEntityType(containerTemplate.getOpalEntityType());
-    variable.setAttributes(attributeTemplate.isPrimaryKey() ? fetchIdAttributes()
-        : fetchAttributes(session, containerTemplate, attributeTemplate));
-    return variable;
-  }
+    private static Variable createVariable(Session session, ContainerTemplate containerTemplate,
+                                           AttributeTemplate attributeTemplate, int counter) {
+        Variable variable = new Variable();
+        variable.setIndex(counter);
+        variable.setName(attributeTemplate.getCsvColumnName());
+        variable.setValueType(attributeTemplate.getOpalValueType());
+        variable.setEntityType(containerTemplate.getOpalEntityType());
+        variable.setAttributes(fetchAttributes(session, containerTemplate, attributeTemplate));
+        return variable;
+    }
 
-  private static List<Attribute> fetchIdAttributes() {
-    Attribute attribute = new Attribute();
-    attribute.setName(ExporterConst.OPAL_ATTRIBUTE_SCRIPT);
-    attribute.setValue(ExporterConst.OPAL_ATTRIBUTE_SCRIPT_ID);
-    return Arrays.asList(attribute);
-  }
+    private static List<Attribute> fetchAttributes(Session session,
+                                                   ContainerTemplate containerTemplate, AttributeTemplate attributeTemplate) {
+        List<Attribute> attributes = new ArrayList<>();
 
-  private static List<Attribute> fetchAttributes(Session session,
-      ContainerTemplate containerTemplate, AttributeTemplate attributeTemplate) {
-    List<Attribute> attributes = new ArrayList<>();
+        Attribute attribute1 = new Attribute();
+        attribute1.setName(ExporterConst.OPAL_ATTRIBUTE_DERIVED_FROM);
+        attribute1.setNamespace(ExporterConst.OPAL_DEFAULT_NAMESPACE);
+        attribute1.setValue(
+                OpalUtils.createVariablePath(session, containerTemplate, attributeTemplate));
+        attributes.add(attribute1);
 
-    Attribute attribute1 = new Attribute();
-    attribute1.setName(ExporterConst.OPAL_ATTRIBUTE_DERIVED_FROM);
-    attribute1.setNamespace(ExporterConst.OPAL_DEFAULT_NAMESPACE);
-    attribute1.setValue(
-        OpalUtils.createVariablePath(session, containerTemplate, attributeTemplate));
-    attributes.add(attribute1);
+        Attribute attribute2 = new Attribute();
+        attribute2.setName(ExporterConst.OPAL_ATTRIBUTE_SCRIPT);
+        attribute2.setValue(fetchScript(attributeTemplate));
+        attributes.add(attribute2);
 
-    Attribute attribute2 = new Attribute();
-    attribute2.setName(ExporterConst.OPAL_ATTRIBUTE_SCRIPT);
-    attribute2.setValue(fetchScript(attributeTemplate));
-    attributes.add(attribute2);
+        return attributes;
+    }
 
-    return attributes;
-  }
+    private static String fetchScript(AttributeTemplate attributeTemplate) {
+        return (attributeTemplate.getOpalScript() != null) ? attributeTemplate.getOpalScript()
+                : fetchDefaultScript(attributeTemplate);
+    }
 
-  private static String fetchScript(AttributeTemplate attributeTemplate) {
-    return (attributeTemplate.getOpalScript() != null) ? attributeTemplate.getOpalScript()
-        : fetchDefaultScript(attributeTemplate);
-  }
-
-  private static String fetchDefaultScript(AttributeTemplate attributeTemplate) {
-    return "${'" + attributeTemplate.getCsvColumnName() + "'}";
-  }
+    private static String fetchDefaultScript(AttributeTemplate attributeTemplate) {
+        return "$('" + attributeTemplate.getCsvColumnName() + "')";
+    }
 
 }
